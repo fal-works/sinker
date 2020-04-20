91494d4d3c898b2b3bc75df186dbf675193075b2
FAL <falworks.contact@gmail.com>
2020-04-21 05:29:18 +0900

    minor

--- test/Main.hx
+++ test/Main.hx
@@ -9,26 +9,26 @@ class Main {
 	}
 
 	static function main() {
-		test((10 : Int).divideBy(2) == 5);
-		test((10 : Int).modulo(3) == 1);
+		test(10.divideBy(2) == 5);
+		test(10.modulo(3) == 1);
 		test(uint(3).powerOf2() == 8);
-		test((3 : Int).square() == 9);
-		test((-3 : Int).absolute() == 3);
+		test(3.square() == 9);
+		test((-3).absolute() == 3);
 
 		test(Floats.toInt(1.5) == 1);
 
 		test(Maybe.from(10).isSome());
 		test(Option.some(1).isSome());
 
-		final a: Array<Int> = [
+		final array: Array<Int> = [
 			1,
 			2,
 			3
 		];
-		test(a.length == 3);
-		var sum = 0;
-		a.forEach(value -> sum += value);
-		test(sum == 6);
+		test(array.length == 3);
+		var arraySum = 0;
+		array.forEach(value -> arraySum += value);
+		test(arraySum == 6);
 
 		trace('Passed all cases.');
 	}
