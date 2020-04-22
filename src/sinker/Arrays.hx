174b5d6c3ddf333ba2a32121f2ef17ba450fbcad
FAL <falworks.contact@gmail.com>
2020-04-22 23:13:18 +0900

    improve error in Arrays.blit/blitZero

--- src/sinker/Arrays.hx
+++ src/sinker/Arrays.hx
@@ -31,9 +31,10 @@ class Arrays {
 	): Void {
 		#if sinker_debug
 		if (source == destination)
-			throw "Failed to blit because source and destination are the same.";
-		if (sourcePosition + rangeLength > source.length) throw "Invalid values.";
-		if (destinationPosition + rangeLength > destination.length) throw "Invalid values.";
+			throw "Failed to blit. Source and destination cannot be the same.";
+		if (sourcePosition + rangeLength > source.length
+			|| destinationPosition + rangeLength > destination.length)
+			throw blitBoundsError(source, sourcePosition, destination, destinationPosition, rangeLength);
 		#end
 
 		#if cpp
@@ -60,9 +61,9 @@ class Arrays {
 	): Void {
 		#if sinker_debug
 		if (source == destination)
-			throw "Failed to blit because source and destination are the same.";
-		if (rangeLength > source.length) throw "Invalid values.";
-		if (rangeLength > destination.length) throw "Invalid values.";
+			throw "Failed to blit. Source and destination cannot be the same.";
+		if (rangeLength > source.length || rangeLength > destination.length)
+			throw blitZeroBoundsError(source, destination, rangeLength);
 		#end
 
 		#if cpp
@@ -75,4 +76,46 @@ class Arrays {
 		}
 		#end
 	}
+
+	#if sinker_debug
+	static function blitBoundsError(
+		source: Array<Dynamic>,
+		sourcePosition: UInt,
+		destination: Array<Dynamic>,
+		destinationPosition: UInt,
+		rangeLength: UInt
+	): String {
+		final message = new StringBuf();
+		message.add('Failed to blit. Out of bounds.');
+		message.add('\nSource length: ');
+		message.add(source.length);
+		message.add(', position: ');
+		message.add(sourcePosition);
+		message.add('\nDestination length: ');
+		message.add(destination.length);
+		message.add(', position: ');
+		message.add(destinationPosition);
+		message.add('\nRange length: ');
+		message.add(rangeLength);
+
+		return message.toString();
+	}
+
+	static function blitZeroBoundsError(
+		source: Array<Dynamic>,
+		destination: Array<Dynamic>,
+		rangeLength: UInt
+	): String {
+		final message = new StringBuf();
+		message.add('Failed to blit. Out of bounds.');
+		message.add('\nSource length: ');
+		message.add(source.length);
+		message.add('\nDestination length: ');
+		message.add(destination.length);
+		message.add('\nRange length: ');
+		message.add(rangeLength);
+
+		return message.toString();
+	}
+	#end
 }
