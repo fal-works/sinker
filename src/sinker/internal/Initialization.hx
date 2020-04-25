36155057961166c94fbd0e3e096e9a95b2e5f209
FAL <falworks.contact@gmail.com>
2020-04-26 07:21:59 +0900

    set initialization macro function to private

--- src/sinker/internal/Initialization.hx
+++ src/sinker/internal/Initialization.hx
@@ -9,7 +9,7 @@ class Initialization {
 
 		Sets compilation flag `sinker_debug` if it is not set and `debug` is set.
 	**/
-	public static function run() {
+	static function run() {
 		#if (debug && !sinker_debug)
 		Compiler.define("sinker_debug", "1");
 		#end
