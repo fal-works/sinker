1f94f0ed299be2fec952d6977742ad4f98e3b404
FAL <falworks.contact@gmail.com>
2022-04-09 01:26:45 +0900

    test: edit Main

--- test/Main.hx
+++ test/Main.hx
@@ -1,5 +1,13 @@
-class Main {
-	static function main() {
-		sinker.Main.main();
-	}
+function main() {
+	printRuntimeInfo();
+	sinker.Main.main();
+}
+
+function printRuntimeInfo() {
+	#if sys
+	final now = Date.now();
+	final target = #if interp "Eval" #elseif hl "HashLink" #elseif js "JavaScript" #else "unknown" #end;
+	Sys.println('time:   $now');
+	Sys.println('target: $target');
+	#end
 }
