dad071491a27735bfa5454c3ade18bef56bb5e58
FAL <falworks.contact@gmail.com>
2022-04-18 20:52:26 +0900

    feat: add @:noUsing to Ints.min/max()

--- src/sinker/Ints.hx
+++ src/sinker/Ints.hx
@@ -47,12 +47,14 @@ class Ints {
 	/**
 		@return The smaller value of `a` and `b`.
 	**/
+	@:noUsing
 	public static extern inline function min(a: Int, b: Int): Int
 		return if (a < b) a else b;
 
 	/**
 		@return The larger value of `a` and `b`.
 	**/
+	@:noUsing
 	public static extern inline function max(a: Int, b: Int): Int
 		return if (a < b) b else a;
 }
